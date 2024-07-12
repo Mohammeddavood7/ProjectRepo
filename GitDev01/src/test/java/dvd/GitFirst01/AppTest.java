2f7151d9083b236dd0315bfb3573f4d80c6d107d
Moham <Moham@Mohammed_Davood>
2024-07-12 15:09:03 +0530

    Dev01 add new method

--- GitDev01/src/test/java/dvd/GitFirst01/AppTest.java
+++ GitDev01/src/test/java/dvd/GitFirst01/AppTest.java
@@ -4,25 +4,29 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.Test;
 
+public class AppTest {
 
-public class AppTest 
-{
-    
-    @Test
-    public void sumPositiveNumber()
-    {
-    	App app = new App();
-    	int expected = 30;
-    	int actual = app.sumOfInteger(10, 20);
-        assertEquals(expected, actual);
-    }
-    
-    @Test
-    public void sumNegativeNumber()
-    {
-    	App app = new App();
-    	int expected = -30;
-    	int actual = app.sumOfInteger(-10, -20);
-        assertEquals(expected, actual);
-    }
+	@Test
+	public void sumPositiveNumber() {
+		App app = new App();
+		int expected = 30;
+		int actual = app.sumOfInteger(10, 20);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void sumNegativeNumber() {
+		App app = new App();
+		int expected = -30;
+		int actual = app.sumOfInteger(-10, -20);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void sumMixedNumber() {
+		App app = new App();
+		int expected = 30;
+		int actual = app.sumOfInteger(-10, 40);
+		assertEquals(expected, actual);
+	}
 }
